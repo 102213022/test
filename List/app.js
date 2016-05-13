7b40dfc2d41a322220f1f1d0460c8b9072badbd0
102213022 <s102213022@mail1.ncnu.edu.tw>
2016-05-13 22:16:22 +0800

    Add files via upload

---
+++ List/app.js
@@ -0,0 +1,102 @@
+angular
+    .module('BoboList',[])
+    .config(config);
+    
+    config.$inject = ['$httpProvider'];
+     function config($httpProvider){
+         $httpProvider.defaults.withCredentials = true;
+     }
+angular
+    .module('BoboList')
+    .controller('ListCtrl',BoboList);
+        
+angular
+    .module('BoboList')
+    .service('List',function($http){
+           this.baseURL = "https://richegg.top";
+           this.tasks = [];
+           this.auth = function(listName){
+                var data = {
+                   listName : listName
+                };
+               
+               return $http.post(this.baseURL+'/lists', data);
+            };
+       
+            this.addTask = function(newTaskText){
+                var data = {
+                    'text' : newTaskText
+                };
+                return $http.post(this.baseURL+'/tasks',data);
+            }
+            this.getTasks = function(){
+                return $http.get(this.baseURL+'/tasks');
+            }
+            
+            this.updateTask = function(task){
+                var data = {
+                    isDone:task.isDone
+                };
+                return $http.patch(this.baseURL+'/tasks/' + task.id,data);
+            };
+            
+            
+            this.deleteTask = function(taskId){
+                return $http.delete(this.baseURL+'/tasks/' + taskId);
+            }
+    });
+        
+    function BoboList(List){
+       var self = this; 
+       
+       
+       
+       self.tasks = [];
+
+       self.auth = function(listName){
+           List.auth(listName)
+               .then(function(res){
+                  self.tasks = List.tasks = res.data.tasks;
+              }),function(err){
+                  console.log(err);
+              };
+       };
+       
+       self.getTasks = function(){
+           //code
+           List.getTasks()
+                    .then(function(res){
+                       self.tasks = List.tasks = res.data.tasks;
+                    }),function(err){
+                        console.log(err);
+                    }
+       };
+       
+       self.addTask = function(newTaskText){
+           List.addTask(newTaskText)
+               .then(function(res){
+                    self.getTasks();
+                }),function(err){
+                    console.log(err);
+                };
+            self.newTaskText="";
+       };
+       
+       self.updateTask = function(task){
+           List.updateTask(task)
+                    .then(function(res){
+                        self.getTasks();
+                    }),function(err){
+                        console.log(err);
+                    }
+       };
+       
+       self.deleteTask = function(taskId){
+           List.deleteTask(taskId)
+                    .then(function(res){
+                        self.getTasks();
+                    }),function(err){
+                        console.log(err);
+                    };
+       };
+    };
